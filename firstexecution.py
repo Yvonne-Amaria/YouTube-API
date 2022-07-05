6ec5a1f828b78b8a02e6df3adc9271e4d45f18f6
Yvonne Obukehwo <yoobukehwo@htu.edu>
2022-07-05 10:15:01 +0000

    update

--- firstexecution.py
+++ firstexecution.py
@@ -5,12 +5,11 @@ api_key = 'AIzaSyA8B7HascL7gfk7MVITyfxOONb1IQzp5N8'
 youtube = build('youtube', 'v3', developerKey=api_key)
 
 request = youtube.channels().list(
-  part = 'statistics',
+  part = 'contentDetails, statistics',
   forUsername = 'sentdex'
 )
 
 response = request.execute()
 
 print(response)
-
 
