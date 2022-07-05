2c4cd9c85971fc9f99957fc770591ccf5f1cf991
Yvonne Obukehwo <yoobukehwo@htu.edu>
2022-07-05 14:06:07 +0000

    Update

--- firstexecution.py
+++ firstexecution.py
@@ -4,12 +4,34 @@ api_key = 'AIzaSyA8B7HascL7gfk7MVITyfxOONb1IQzp5N8'
 
 youtube = build('youtube', 'v3', developerKey=api_key)
 
-request = youtube.channels().list(
+detailsrequest = youtube.channels().list(
   part = 'contentDetails, statistics',
   forUsername = 'sentdex'
 )
 
-response = request.execute()
+playlistrequest = youtube.playlists().list(
+  part = 'contentDetails, snippet',
+  channelId = "UCfzlCWGWYyIQ0aLC5w48gBQ"
+)
 
-print(response)
+oneplaylistrequest = youtube.playlistItems().list(
+  part = 'contentDetails',
+  playlistId = "PLQVvvaa0QuDcBby2qVDsDv41GghEQfr5E"
+)
 
+detailsresponse = detailsrequest.execute()
+playlistresponse = playlistrequest.execute()
+oneplaylistresponse = oneplaylistrequest.execute()
+
+print(detailsresponse)
+print(playlistresponse)
+print(oneplaylistresponse)
+
+for item in playlistresponse['items']:
+  print(item)
+  print()
+
+for item in oneplaylistresponse['items']:
+  videoId = item['contentDetails']['videoId']
+  print(videoId)
+  print()
